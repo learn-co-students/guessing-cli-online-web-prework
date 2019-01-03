458da074af35a33281172097bdb3d4ab992cc175
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 20:35:26 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,9 +3,9 @@ def run_guessing_game
 user_input = ""
   while user_input
       puts "Guess a number between 1 and 6."
-      pcans = 1+rand(6)
+      pcans1 = rand(1..6).to_s
       user_input= gets.chomp
-
+      pcans=pcans1
     if  user_input == "exit"
         puts  "Goodbye!"
           break
