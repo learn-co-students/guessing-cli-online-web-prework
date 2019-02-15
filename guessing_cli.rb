c4bed603e92282dc5d101f02c4e229b40f66091f
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-15 23:04:33 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,10 +3,15 @@ require 'pry'
 
 
 def run_guessing_game(input)
+  number = number_generator
   while input != "exit"
     
-  
+    
   
   end 
   puts "Goodbye!"
+end 
+
+def number_generator
+  rand(1..6)
 end 
