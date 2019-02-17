4c1ecf877daaa865057eb30a76ad0a9a7db96009
Mark Kanko <mkanko66@gmail.com>
2019-02-17 08:02:21 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -17,3 +17,17 @@ def run_guessing_game
   
   end 
 end 
+
+
+
+# rand(6)    #=> gives a random number between 0 and 6 inclusively 
+# rand(1..6) #=> gives a random number between 1 and 6 inclusively
+
+# While you can use rand(42-10) + 10 to get a random number between 10 and 42 
+# (where 10 is inclusive and 42 exclusive), there's a better way since 
+# Ruby 1.9.3, where you are able to call:
+# rand(10...42) # => 13
+
+# If you needed a random integer to simulate a roll of a six-sided die, 
+# you'd use: 1 + rand(6). A roll in craps could be simulated with 
+# 2 + rand(6) + rand(6).
