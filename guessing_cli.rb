99c442de0ce4d83ea9366639c8bc0d342b7d6ef3
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-17 01:29:00 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,17 +1,22 @@
 # Code your solution here!
 require 'pry'
 
-
-def run_guessing_game(input)
-  number = number_generator
-  while input != "exit"
-    
-    
-  
-  end 
-  puts "Goodbye!"
-end 
-
 def number_generator
   rand(1..6)
 end 
+
+def run_guessing_game
+  loop do 
+    puts "Guess a number between 1 and 6."
+    input = gets.chomp
+    number = number_generator
+      if input.downcase == 'exit'
+        puts "Goodbye!"
+        break 
+      elsif input.to_i == number 
+        puts "You guessed the correct number!"
+      elsif input.to_i != number 
+        puts "The computer guessed #{number}."
+      end 
+  end 
+end
